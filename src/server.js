0cfa8ff4701ee84fb0a58e5141effb794c8417aa
Ya Min Mo Mo <yaminmomo.15@gmail.com>
2024-11-18 16:41:33 -0500

    initial commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}); 
